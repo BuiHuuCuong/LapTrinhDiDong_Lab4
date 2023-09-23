acd463f3cb4dc0ea24036d7ed40a920787feb2a1
BuiHuuCuong <buihuucuong2002@gmail.com>
2023-09-23 13:47:20 +0700

    BT4 - Second Commit

--- app/src/main/java/com/example/lab4/MainActivity.java
+++ app/src/main/java/com/example/lab4/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    // Demo
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
